7fc1b5a5f9db00ab28821a5d1d3b4342101c65de
aseg3092 <gabrielsalazar3092@gmail.com>
2022-02-21 16:10:45 +0000

    navbar

--- src/js/component/NavBar.jsx
+++ src/js/component/NavBar.jsx
@@ -5,20 +5,12 @@ const NavBar = () => {
 		<nav className="navbar navbar-expand-lg navbar-light bg-light">
 			<div className="container-fluid">
 				<a className="navbar-brand" href="#">
-					Navbar
+					Bootstrap
 				</a>
-				<button
-					className="navbar-toggler"
-					type="button"
-					data-bs-toggle="collapse"
-					data-bs-target="#navbarNav"
-					aria-controls="navbarNav"
-					aria-expanded="false"
-					aria-label="Toggle navigation">
-					<span className="navbar-toggler-icon"></span>
-				</button>
-				<div className="collapse navbar-collapse" id="navbarNav">
-					<ul className="navbar-nav">
+				<div
+					className="collapse navbar-collapse d-flex justify-content-end"
+					id="navbarNav">
+					<ul className="nav ">
 						<li className="nav-item">
 							<a
 								className="nav-link active"
@@ -29,16 +21,16 @@ const NavBar = () => {
 						</li>
 						<li className="nav-item">
 							<a className="nav-link" href="#">
-								Features
+								About
 							</a>
 						</li>
 						<li className="nav-item">
 							<a className="nav-link" href="#">
-								Pricing
+								Services
 							</a>
 						</li>
 						<li className="nav-item">
-							<a className="nav-link disabled">Disabled</a>
+							<a className="nav-link ">Contact</a>
 						</li>
 					</ul>
 				</div>
